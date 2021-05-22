ab7f1f53ca269e3cab81ec17b21c760a8ad16a85
GZMaster <danigamester@gmail.com>
2021-05-22 01:50:22 +0100

    Create passdoorcode
    this is the code for the IC program

---
+++ passdoorCode.c
@@ -0,0 +1,27 @@
+#include<reg51.h>
+
+//for 11.0592MHz
+void delay(unsigned int count)
+{
+	unsigned int i;
+	while(count)
+	{
+		i = 115;
+		while(i > 0)
+			i--;
+		count--;
+	}
+}
+
+
+
+//main function called
+void main ()
+{
+
+	while(1)
+	{
+
+	}
+
+}
