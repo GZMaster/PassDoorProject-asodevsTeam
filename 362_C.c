0dfdca562ee1648d8db0df24a9ef9bf7b2c5e352
Apprentice-doa <85044207+Apprentice-doa@users.noreply.github.com>
2021-07-13 12:53:16 +0100

    Add files via upload

---
+++ 362_C.c
@@ -0,0 +1,8 @@
+/*A Program to delare my interest in C language*/
+# include <stdio.h>
+# include <stdlib.h>
+int main()
+{
+	printf("Personal marathon in C till exam");//current position
+	return 0;
+}
